f1d1afe914ee6b7f5fc5bbb06f3190aa0d45c73d
jvmichelotti <jvbulegon@gmail.com>
2025-07-06 10:44:04 -0300

    @if e @for

--- src/app/rooms/rooms.ts
+++ src/app/rooms/rooms.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Room, RoomList } from './roomsInterface';
 
 @Component({
   selector: 'hinv-rooms',
@@ -14,6 +15,42 @@ export class Rooms {
   numberofRooms = 10;
 
   hideRooms = false;
+
+  rooms: Room = {
+    totalRooms: 20,
+    availableRooms: 10,
+    bookedRooms: 5
+  }
+
+  roomList: RoomList[] = [{
+    roomNumber: 1,
+    roomType: "Deluxe Room",
+    amenities: "Air Conditioner, Free wi-fi, TV",
+    price: 500,
+    photos: "https://picsum.photos/200/300",
+    checkInTime: new Date("11-Nov-2025"),
+    checkOutTime: new Date("12-Nov-2025")
+
+  },
+  {
+    roomNumber : 2,
+    roomType: "Deluxe Room 2",
+    amenities: "Air Conditioner, Free wi-fi, TV, pool",
+    price: 700,
+    photos: "https://picsum.photos/200/300",
+    checkInTime: new Date("12-Nov-2025"),
+    checkOutTime: new Date("13-Nov-2025")
+  },
+  {
+    roomNumber : 3,
+    roomType: "Deluxe Room 3",
+    amenities: "Air Conditioner, Free wi-fi, TV, pool",
+    price: 700,
+    photos: "https://picsum.photos/200/300",
+    checkInTime: new Date("14-Nov-2025"),
+    checkOutTime: new Date("15-Nov-2025")
+  }
+  ]
 
   toggle() {
     this.hideRooms = !this.hideRooms;
